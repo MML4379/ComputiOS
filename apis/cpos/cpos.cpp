f2422bce4aef1e4e5735f0c1d3def1c3b6809ff5
Michael Peace <76636013+MML4379@users.noreply.github.com>
2025-11-28 18:40:23 -0500

    Delete apis directory

--- apis/cpos/cpos.cpp
+++
@@ -1,7 +0,0 @@
-// ComputiOS API
-// Copyright © 2025 MML Tech LLC
-// Licensed Under the ComputiOS License
-
-// This API lets applications, subroutines, extensions, and the end user interact with the ComputiOS kernel and its drivers.
-
-#include "cpos.hpp"
--- apis/cpos/cpos.hpp
+++
@@ -1,8 +0,0 @@
-// ComputiOS API
-// Copyright © 2025 MML Tech LLC
-// Licensed Under the ComputiOS License
-
-// This API lets applications, subroutines, extensions, and the end user interact with the ComputiOS kernel and its drivers.
-
-#include "../../kernel/cposapi/kernel.hpp"
-#include "../libc/libc.h"
